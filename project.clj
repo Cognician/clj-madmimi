c6e2fd644bd3f9527e531d3f42baef1352529641
Robert Stuttaford <robert@cognician.com>
2013-07-29 12:54:30 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-madmimi "0.1.1"
+(defproject clj-madmimi "0.1.2"
   :description "Clojure interface for Mad Mimi email service"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.6.0"]]
