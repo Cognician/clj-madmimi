3714e91c51b91bd5ecbbd3d837d032be5eaaf7dd
Robert Stuttaford <robert@cognician.com>
2013-07-29 12:53:55 +0200

    Loosen assertion and add optional reply-to handling

--- src/clj_madmimi/core.clj
+++ src/clj_madmimi/core.clj
@@ -9,14 +9,21 @@
 
 (defn mad-mimi
   [api-key username from mail]
-  {:pre [(every? #{:promotion :to :subject :html} (keys mail))]}
-  (send-mail {:api_key api-key
-              :username username
-              :from from
-              :promotion_name (:promotion mail)
-              :recipient (:to mail)
-              :subject (:subject mail)
-              :raw_html (:html mail)}))
+  {:pre [(and (:promotion mail)
+              (:to mail)
+              (:subject mail)
+              (:html mail))]}
+  (let [to-send {:api_key api-key
+                 :username username
+                 :from from
+                 :promotion_name (:promotion mail)
+                 :recipient (:to mail)
+                 :subject (:subject mail)
+                 :raw_html (:html mail)}
+        to-send (if-let [reply-to (:reply-to mail)]
+                  (assoc to-send :reply_to reply-to)
+                  to-send)]
+    (send-mail to-send)))
 
 (defn make-mad-mimi
   [api-key username from]
